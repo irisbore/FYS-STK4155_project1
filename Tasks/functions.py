b4afadf8059dab9c840c982234a4b62d580f28c3
irisbore <irisbore@gmail.com>
2024-09-17 13:43:01 +0200

    type hinting, new design matrix

--- Tasks/functions.py
+++ Tasks/functions.py
@@ -2,75 +2,25 @@ import numpy as np
 
 # Franke function
 
-def FrankeFunction(x,y):
+def FrankeFunction(x: np.meshgrid,y: np.meshgrid) -> np.array:
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+
     #Noise term
-    term5 = np.random.normal(0, 0.1, len(x))
-    return term1 + term2 + term3 + term4 + term5
+    #term5 = np.random.normal(0, 0.1, len(x))
+    return term1 + term2 + term3 + term4 #+ term5
 
 
 # Design matrix
-def create_design_matrix(x, y, degree):
-    X = np.zeros((len(x), degree+1))
-    if degree == 0:
-        X[:, 0] = x
-    elif degree == 1:
-        X[:, 0] = x
-        X[:, 1] = y
-    elif degree == 2:
-        X[:, 0] = x
-        X[:, 1] = y
-        X[:, 2] = x@x.T
-    elif degree == 3:
-        X[:, 0] = x
-        X[:, 1] = y
-        X[:, 2] = x@x.T
-        X[:, 3] = y@y.T
-    elif degree == 4:
-        X[:, 0] = x
-        X[:, 1] = y
-        X[:, 2] = x@x.T
-        X[:, 3] = y@y.T
-        X[:, 4] = x@y.T
-    else:
-        print("Degree not implemented")
+def create_design_matrix(x: np.array, y: np.array, p: int) -> np.array:
+    X = np.zeros((len(x), int(1/2 * p* (p+3))))
+    col_index = 0
+    for i in range(p):
+        for j in range(p-i):
+            if i+j != 0:
+                X[:, col_index] = x**i + y**j
+                print(f"X[:, {col_index}], x**{i} + y**{j}")
+                col_index += 1
     return X
-
-"""
-# Design matrix
-def create_design_matrix(x, y, degree):
-    X = np.zeros((len(x), degree+1))
-    if degree == 0:
-        X[:, 0] = 1.0
-    elif degree == 1:
-        X[:, 0] = 1.0
-        X[:, 1] = x
-    elif degree == 2:
-        X[:, 0] = 1.0
-        X[:, 1] = x
-        X[:, 2] = y
-    elif degree == 3:
-        X[:, 0] = 1.0
-        X[:, 1] = x
-        X[:, 2] = y
-        X[:, 3] = x@x.T
-    elif degree == 4:
-        X[:, 0] = 1.0
-        X[:, 1] = x
-        X[:, 2] = y
-        X[:, 3] = x@x.T
-        X[:, 4] = y@y.T
-    elif degree == 5:
-        X[:, 0] = 1.0
-        X[:, 1] = x
-        X[:, 2] = y
-        X[:, 3] = x@x.T
-        X[:, 4] = y@y.T
-        X[:, 5] = x@y.T
-    else:
-        print("Degree not implemented")
-    return X
-    """
